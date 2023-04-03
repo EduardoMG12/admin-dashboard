25a84f9d063c6eacc59d6339735d74cf6f20e546
Eduardo Mello <102185386+EduardoMG12@users.noreply.github.com>
2023-04-03 18:19:27 -0300

    Feat/navbar (#3)
    * feat(context):added more contexts
    
    * feat(navbar):created navbar and your logic
    
    * feat(context): created new state on context for ask screen width
    
    * feat:implementated screen width for dont show menu
    
    * refactor(sizeIcons): fixed size icons
    
    ---------
    
    Co-authored-by: Eduardo <eduardo@pop-os.localdomain>

--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -10,18 +10,33 @@ interface IState {
 interface IStateContext {
 	activeMenu: boolean;
 	setActiveMenu: (value: boolean) => void;
-	state: IState;
+	isClicked: IState;
+	setIsClicked: (value: IState) => void;
+	handleClick: (clicked: keyof IState) => void;
+	screenSize: number | undefined;
+	setScreenSize: React.Dispatch<React.SetStateAction<number | undefined>>;
 }
+
+const initialState: IState = {
+	chat: false,
+	cart: false,
+	userProfile: false,
+	notification: false,
+};
 
 const StateContext = createContext<IStateContext>({
 	activeMenu: true,
 	setActiveMenu: () => {},
-	state: {
+	isClicked: {
 		chat: false,
 		cart: false,
 		userProfile: false,
 		notification: false,
 	},
+	setIsClicked: () => {},
+	handleClick: () => {},
+	screenSize: 1920,
+	setScreenSize: () => {},
 });
 
 interface IPropsContextProvider {
@@ -32,16 +47,26 @@ export const ContextProvider: React.FC<IPropsContextProvider> = ({
 	children,
 }) => {
 	const [activeMenu, setActiveMenu] = useState<boolean>(true);
-	const [state, setState] = useState<IState>({
-		chat: false,
-		cart: false,
-		userProfile: false,
-		notification: false,
-	});
+	const [isClicked, setIsClicked] = useState<IState>(initialState);
+	const [screenSize, setScreenSize] = useState<number | undefined>(1920);
+
+	const handleClick = (clicked: keyof IState) => {
+		setIsClicked({ ...initialState, [clicked]: true });
+	};
 
 	return (
 		<>
-			<StateContext.Provider value={{ activeMenu, setActiveMenu, state }}>
+			<StateContext.Provider
+				value={{
+					activeMenu,
+					setActiveMenu,
+					isClicked,
+					setIsClicked,
+					handleClick,
+					screenSize,
+					setScreenSize,
+				}}
+			>
 				{children}
 			</StateContext.Provider>
 		</>
