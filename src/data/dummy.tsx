c1660f4a0caa9cd70ad2ae0a6a8aed995a6d6222
Eduardo <eduardo@pop-os.localdomain>
2023-04-06 01:30:25 -0300

    refactor(dummy/data): changed/fixed interface for stackedPrimary YAxis and XAxis

--- src/data/dummy.tsx
+++ src/data/dummy.tsx
@@ -6,23 +6,8 @@ import {
 	AiOutlineBarChart,
 	AiOutlineStock,
 } from "react-icons/ai";
-import {
-	FiShoppingBag,
-	FiEdit,
-	FiPieChart,
-	FiBarChart,
-	FiCreditCard,
-	FiStar,
-	FiShoppingCart,
-} from "react-icons/fi";
-import {
-	BsKanban,
-	BsBarChart,
-	BsBoxSeam,
-	BsCurrencyDollar,
-	BsShield,
-	BsChatLeft,
-} from "react-icons/bs";
+import { FiShoppingBag, FiEdit, FiPieChart, FiBarChart, FiCreditCard, FiStar, FiShoppingCart } from "react-icons/fi";
+import { BsKanban, BsBarChart, BsBoxSeam, BsCurrencyDollar, BsShield, BsChatLeft } from "react-icons/bs";
 import { BiColorFill } from "react-icons/bi";
 import { IoMdContacts } from "react-icons/io";
 import { RiContactsLine, RiStockLine } from "react-icons/ri";
@@ -43,14 +28,11 @@ import product5 from "./product5.jpg";
 import product6 from "./product6.jpg";
 import product7 from "./product7.jpg";
 import product8 from "./product8.jpg";
+import { AxisModel } from "@syncfusion/ej2-react-charts";
 
 export const gridOrderImage = (props: any) => (
 	<div>
-		<img
-			className="rounded-xl h-20 md:ml-3"
-			src={props.ProductImage}
-			alt="order-item"
-		/>
+		<img className="rounded-xl h-20 md:ml-3" src={props.ProductImage} alt="order-item" />
 	</div>
 );
 
@@ -80,11 +62,7 @@ export const kanbanGrid = [
 ];
 const gridEmployeeProfile = (props: any) => (
 	<div className="flex items-center gap-2">
-		<img
-			className="rounded-full w-10 h-10"
-			src={props.EmployeeImage}
-			alt="employee"
-		/>
+		<img className="rounded-full w-10 h-10" src={props.EmployeeImage} alt="employee" />
 		<p>{props.Name}</p>
 	</div>
 );
@@ -98,56 +76,38 @@ const gridEmployeeCountry = (props: any) => (
 export const EditorData = () => (
 	<div>
 		<h3>
-			Try React React has been designed from the start for gradual adoption, and
-			you can use as little or as much React as you need. Whether you want to
-			get a taste of React, add some interactivity to a simple HTML page, or
-			start a complex React-powered app, the links in this section will help you
-			get started. Online Playgrounds If you’re interested in playing around
-			with React, you can use an online code playground. Try a Hello World
-			template on CodePen, CodeSandbox, or Stackblitz. If you prefer to use your
-			own text editor, you can also download this HTML file, edit it, and open
-			it from the local filesystem in your browser. It does a slow runtime code
-			transformation, so we’d only recommend using this for simple demos. Add
-			React to a Website You can add React to an HTML page in one minute. You
-			can then either gradually expand its presence, or keep it contained to a
-			few dynamic widgets. Create a New React App When starting a React project,
-			a simple HTML page with script tags might still be the best option. It
-			only takes a minute to set up! As your application grows, you might want
-			to consider a more integrated setup. There are several JavaScript
-			toolchains we recommend for larger applications. Each of them can work
-			with little to no configuration and lets you take full advantage of the
-			rich React ecosystem. Learn how. Learn React People come to React from
-			different backgrounds and with different learning styles. Whether you
-			prefer a more theoretical or a practical approach, we hope you’ll find
-			this section helpful. If you prefer to learn by doing, start with our
-			practical tutorial. If you prefer to learn concepts step by step, start
-			with our guide to main concepts. Like any unfamiliar technology, React
-			does have a learning curve. With practice and some patience, you will get
-			the hang of it. First Examples The React homepage contains a few small
-			React examples with a live editor. Even if you don’t know anything about
-			React yet, try changing their code and see how it affects the result.
-			React for Beginners If you feel that the React documentation goes at a
-			faster pace than you’re comfortable with, check out this overview of React
-			by Tania Rascia. It introduces the most important React concepts in a
-			detailed, beginner-friendly way. Once you’re done, give the documentation
-			another try! React for Designers If you’re coming from a design
-			background, these resources are a great place to get started. JavaScript
-			Resources The React documentation assumes some familiarity with
-			programming in the JavaScript language. You don’t have to be an expert,
-			but it’s harder to learn both React and JavaScript at the same time. We
-			recommend going through this JavaScript overview to check your knowledge
-			level. It will take you between 30 minutes and an hour but you will feel
-			more confident learning React.
+			Try React React has been designed from the start for gradual adoption, and you can use as little or as much React
+			as you need. Whether you want to get a taste of React, add some interactivity to a simple HTML page, or start a
+			complex React-powered app, the links in this section will help you get started. Online Playgrounds If you’re
+			interested in playing around with React, you can use an online code playground. Try a Hello World template on
+			CodePen, CodeSandbox, or Stackblitz. If you prefer to use your own text editor, you can also download this HTML
+			file, edit it, and open it from the local filesystem in your browser. It does a slow runtime code transformation,
+			so we’d only recommend using this for simple demos. Add React to a Website You can add React to an HTML page in
+			one minute. You can then either gradually expand its presence, or keep it contained to a few dynamic widgets.
+			Create a New React App When starting a React project, a simple HTML page with script tags might still be the best
+			option. It only takes a minute to set up! As your application grows, you might want to consider a more integrated
+			setup. There are several JavaScript toolchains we recommend for larger applications. Each of them can work with
+			little to no configuration and lets you take full advantage of the rich React ecosystem. Learn how. Learn React
+			People come to React from different backgrounds and with different learning styles. Whether you prefer a more
+			theoretical or a practical approach, we hope you’ll find this section helpful. If you prefer to learn by doing,
+			start with our practical tutorial. If you prefer to learn concepts step by step, start with our guide to main
+			concepts. Like any unfamiliar technology, React does have a learning curve. With practice and some patience, you
+			will get the hang of it. First Examples The React homepage contains a few small React examples with a live editor.
+			Even if you don’t know anything about React yet, try changing their code and see how it affects the result. React
+			for Beginners If you feel that the React documentation goes at a faster pace than you’re comfortable with, check
+			out this overview of React by Tania Rascia. It introduces the most important React concepts in a detailed,
+			beginner-friendly way. Once you’re done, give the documentation another try! React for Designers If you’re coming
+			from a design background, these resources are a great place to get started. JavaScript Resources The React
+			documentation assumes some familiarity with programming in the JavaScript language. You don’t have to be an
+			expert, but it’s harder to learn both React and JavaScript at the same time. We recommend going through this
+			JavaScript overview to check your knowledge level. It will take you between 30 minutes and an hour but you will
+			feel more confident learning React.
 		</h3>
 	</div>
 );
 const customerGridImage = (props: any) => (
 	<div className="image flex gap-4">
-		<img
-			className="rounded-full w-10 h-10"
-			src={props.CustomerImage}
-			alt="employee"
-		/>
+		<img className="rounded-full w-10 h-10" src={props.CustomerImage} alt="employee" />
 		<div>
 			<p>{props.CustomerName}</p>
 			<p>{props.CustomerEmail}</p>
@@ -157,10 +117,7 @@ const customerGridImage = (props: any) => (
 
 const customerGridStatus = (props: any) => (
 	<div className="flex gap-2 justify-center items-center text-gray-700 capitalize">
-		<p
-			style={{ background: props.StatusBg }}
-			className="rounded-full h-3 w-3"
-		/>
+		<p style={{ background: props.StatusBg }} className="rounded-full h-3 w-3" />
 		<p>{props.Status}</p>
 	</div>
 );
@@ -2125,8 +2082,7 @@ export const ordersData = [
 		Location: "USA",
 		Status: "pending",
 		StatusBg: "#FB9678",
-		ProductImage:
-			"https://cdn.shopclues.com/images1/thumbnails/104158/320/320/148648730-104158193-1592481791.jpg",
+		ProductImage: "https://cdn.shopclues.com/images1/thumbnails/104158/320/320/148648730-104158193-1592481791.jpg",
 	},
 	{
 		OrderID: 874534,
@@ -2261,8 +2217,7 @@ export const ordersData = [
 		Location: "USA",
 		Status: "pending",
 		StatusBg: "#FB9678",
-		ProductImage:
-			"https://cdn.shopclues.com/images1/thumbnails/104158/320/320/148648730-104158193-1592481791.jpg",
+		ProductImage: "https://cdn.shopclues.com/images1/thumbnails/104158/320/320/148648730-104158193-1592481791.jpg",
 	},
 	{
 		OrderID: 874534,
@@ -2397,8 +2352,7 @@ export const ordersData = [
 		Location: "USA",
 		Status: "pending",
 		StatusBg: "#FB9678",
-		ProductImage:
-			"https://cdn.shopclues.com/images1/thumbnails/104158/320/320/148648730-104158193-1592481791.jpg",
+		ProductImage: "https://cdn.shopclues.com/images1/thumbnails/104158/320/320/148648730-104158193-1592481791.jpg",
 	},
 	{
 		OrderID: 874534,
@@ -2533,8 +2487,7 @@ export const ordersData = [
 		Location: "USA",
 		Status: "pending",
 		StatusBg: "#FB9678",
-		ProductImage:
-			"https://cdn.shopclues.com/images1/thumbnails/104158/320/320/148648730-104158193-1592481791.jpg",
+		ProductImage: "https://cdn.shopclues.com/images1/thumbnails/104158/320/320/148648730-104158193-1592481791.jpg",
 	},
 	{
 		OrderID: 874534,
@@ -2669,8 +2622,7 @@ export const ordersData = [
 		Location: "USA",
 		Status: "pending",
 		StatusBg: "#FB9678",
-		ProductImage:
-			"https://cdn.shopclues.com/images1/thumbnails/104158/320/320/148648730-104158193-1592481791.jpg",
+		ProductImage: "https://cdn.shopclues.com/images1/thumbnails/104158/320/320/148648730-104158193-1592481791.jpg",
 	},
 	{
 		OrderID: 874534,
@@ -3074,7 +3026,7 @@ export const stackedCustomSeries = [
 	},
 ];
 
-export const stackedPrimaryXAxis = {
+export const stackedPrimaryXAxis: AxisModel = {
 	majorGridLines: { width: 0 },
 	minorGridLines: { width: 0 },
 	majorTickLines: { width: 0 },
@@ -3085,7 +3037,7 @@ export const stackedPrimaryXAxis = {
 	valueType: "Category",
 };
 
-export const stackedPrimaryYAxis = {
+export const stackedPrimaryYAxis: AxisModel = {
 	lineStyle: { width: 0 },
 	minimum: 100,
 	maximum: 400,
