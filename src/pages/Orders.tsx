c62f8a2fb1f2704e1a6a13c2bc2abd081971d76a
Eduardo <eduardo@pop-os.localdomain>
2023-04-06 13:36:33 -0300

    feat(page/order): added filter sorting

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -20,7 +20,7 @@ const Orders: React.FC = () => {
 	return (
 		<div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
 			<Header category="Page" title="Orders" />
-			<GridComponent id="gridcomp" dataSource={ordersData} allowPaging>
+			<GridComponent id="gridcomp" dataSource={ordersData} allowPaging allowSorting>
 				<ColumnsDirective>
 					{ordersGrid.map((item, index) => (
 						<ColumnDirective key={index} {...item} />
