fdff87809ea72d4fa78c88522c83eea9093174ac
Eduardo Mello <102185386+EduardoMG12@users.noreply.github.com>
2023-04-08 11:31:32 -0300

    Hotfix (#8)
    * refactor(index): remove margin
    
    * shore: added configurations
    
    * added dependencies
    
    * refator():implemented .env
    
    ---------
    
    Co-authored-by: Eduardo <eduardo@pop-os.localdomain>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsConfigPaths from 'vite-tsconfig-paths'
-
+import "dotenv/config"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsConfigPaths()],
+  define:{
+    
+  }
 })
