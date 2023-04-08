7b85ef19903406f906edeeb047f126c6c3437e60
Eduardo <eduardo@pop-os.localdomain>
2023-04-08 11:30:28 -0300

    refator():implemented .env

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ContextProvider } from "./contexts/ContextProvider";
+import { registerLicense } from "@syncfusion/ej2-base";
 
+registerLicense(import.meta.env.VITE_KEY);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<ContextProvider>
