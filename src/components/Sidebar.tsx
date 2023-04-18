ccd21a6d844f633f2ec47ed48913ce8eeb5b6240
Eduardo <eduardo@pop-os.localdomain>
2023-04-18 15:00:13 -0300

    refactor(Menus/Sidebar):fixed dark mode

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,12 +12,12 @@ const Sidebar: React.FC = () => {
 	const handleCloseSideBar = () => {
 		if (activeMenu && screenSize && screenSize <= 900) setActiveMenu(false);
 	};
-	const activeLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg texto-white text-md m-2";
+	const activeLink = "flex items-center font-bold gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
 	const normalLink =
-		"flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg texto-white text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
+		"flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-black text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
 
 	return (
-		<div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+		<div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10 dark:bg-main-dark-bg">
 			{activeMenu && (
 				<>
 					<div className="flex justify-between items-center">
