a3702cf2d48f5dbca2eaea788fb6b301a148645a
Eduardo <eduardo@pop-os.localdomain>
2023-04-18 14:23:30 -0300

    feat(Sidebar): added validation

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,13 +7,12 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar: React.FC = () => {
-	const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+	const { activeMenu, setActiveMenu, screenSize, currentColor } = useStateContext();
 
 	const handleCloseSideBar = () => {
 		if (activeMenu && screenSize && screenSize <= 900) setActiveMenu(false);
 	};
-	const activeLink =
-		"flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg texto-white text-md m-2";
+	const activeLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg texto-white text-md m-2";
 	const normalLink =
 		"flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg texto-white text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
 
@@ -42,19 +41,15 @@ const Sidebar: React.FC = () => {
 					</div>
 					<div className="mt-10">
 						{links.map((item) => (
-							<div
-								className="text-gray-400 m-3 mt-4 uppercase"
-								key={item.title}
-							>
+							<div className="text-gray-400 m-3 mt-4 uppercase" key={item.title}>
 								<p className="text-gray-400 m-3 mt-4 uppercase">{item.title}</p>
 								{item.links.map((link) => (
 									<NavLink
 										to={`/${link.name}`}
 										key={link.name}
 										onClick={handleCloseSideBar}
-										className={({ isActive }) =>
-											isActive ? activeLink : normalLink
-										}
+										style={({ isActive }) => ({ backgroundColor: isActive ? currentColor : "" })}
+										className={({ isActive }) => (isActive ? activeLink : normalLink)}
 									>
 										{link.icon}
 										<span className="capitalize">{link.name}</span>
