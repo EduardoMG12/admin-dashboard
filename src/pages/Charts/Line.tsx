9097280a1fb382e688e14cce3467639259522ec1
Eduardo <eduardo@pop-os.localdomain>
2023-04-19 15:09:42 -0300

    refactor(Line/Chart): refactored lineChart added ChartHeader

--- src/pages/Charts/Line.tsx
+++ src/pages/Charts/Line.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { Header, LineChart } from "../../components";
 
-const Line: React.FC = () => {
-	return (
-		<div className="m-4 nd:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg dark:text-white rounded-3xl">
-			<Header category="Chart" title="Inflation Rate" />
-			Line
-			<div className="w-full">
-				<LineChart />
-			</div>
+import { ChartsHeader, LineChart } from "../../components";
+
+const Line: React.FC = () => (
+	<div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+		<ChartsHeader category="Line" title="Inflation Rate" />
+		<div className="w-full">
+			<LineChart />
 		</div>
-	);
-};
+	</div>
+);
 
 export default Line;
