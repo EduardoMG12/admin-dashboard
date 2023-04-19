865eedb753160e082673c4e2769e58ace5567683
Eduardo <eduardo@pop-os.localdomain>
2023-04-19 14:40:22 -0300

    feat(LineChart): created lineChart

--- src/pages/Charts/Line.tsx
+++ src/pages/Charts/Line.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Header, LineChart } from "../../components";
 
 const Line: React.FC = () => {
-	return <div>Line</div>;
+	return (
+		<div className="m-4 nd:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg dark:text-white rounded-3xl">
+			<Header category="Chart" title="Inflation Rate" />
+			Line
+			<div className="w-full">
+				<LineChart />
+			</div>
+		</div>
+	);
 };
 
 export default Line;
